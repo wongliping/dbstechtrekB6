f90b91b543229e8a684ed4e4defcaa6ca84cbcac
wongliping <wong0997@e.ntu.edu.sg>
2019-02-01 15:16:09 +0800

    template

--- DBS Techtrek/script.js
+++ DBS Techtrek/script.js
@@ -1,11 +1,11 @@
-// function init() {
-//     'use strict';
+function init() {
+    'use strict';
 
-//     // Confirm that document.getElementById() can be used:
-//     if (document && document.getElementById) {
+    // Confirm that document.getElementById() can be used:
+    if (document && document.getElementById) {
     	
 
-// 	}	 // End of init() function.
-// }
-// // Assign an event listener to the window's load event:
-// window.onload = init();
+	}	 // End of init() function.
+}
+// Assign an event listener to the window's load event:
+window.onload = init();
